b99dd9c5c821dc748efbe1ff98bfe8220c2799ba
Zach Barbre <zach.barbre@gmail.com>
2020-05-14 15:13:46 -0400

    finish header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import Guesses from './components/Guesses';
+import Header from './components/Header';
 
 const App = () => {
   
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Baby Byrd will...</h1>
-        <p>Be born on what day? Weigh how much? Be a girl or boy?</p>
-        <p>See the guesses below!</p>
-      </header>
+      <Header />
       <Guesses />
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,38 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// const StyledHeader = styled.header`
-//   font: 
-// `;
+const StyledHeader = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: var(--primary);
+  color: var(--dark);
+  
+  h1 {
+    font-family: 'Yellowtail', cursive;
+    font-size: 3.5rem;
+  }
+
+  .subheader {
+   width: 80%;
+   background-color: var(--primary-light);
+   margin-bottom: 5%;
+   padding: 0 5%;
+   border-radius: 5%;
+   text-align: center;
+  }
+`;
 
 const Header = () => {
   return(
-    <header>
+    <StyledHeader>
       <h1>Baby Byrd will...</h1>
-      <p>Be born on what day? Weigh how much? Be a girl or boy?</p>
-      <p>See the guesses below!</p>
-    </header>
+      <div className="subheader">
+        <p>Be born on what day? Weigh how much? Be a girl or boy?</p>
+        <p>See the guesses below!</p>
+      </div>
+    </StyledHeader>
   );
 };
 
