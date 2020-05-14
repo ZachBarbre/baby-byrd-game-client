1647af7bc2e04660f7b493aa4ac88c7a47831ec3
Zach Barbre <zach.barbre@gmail.com>
2020-05-14 11:48:22 -0400

    create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+
+// const StyledHeader = styled.header`
+//   font: 
+// `;
+
+const Header = () => {
+  return(
+    <header>
+      <h1>Baby Byrd will...</h1>
+      <p>Be born on what day? Weigh how much? Be a girl or boy?</p>
+      <p>See the guesses below!</p>
+    </header>
+  );
+};
+
+export default Header;
