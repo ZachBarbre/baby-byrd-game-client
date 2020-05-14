ec390f164e76742669096fce30a0ac5a70975905
Zach Barbre <zach.barbre@gmail.com>
2020-05-14 10:33:17 -0400

    clean create-react-app boilerplate, add basic header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Baby Byrd will...</h1>
+        <p>Be born on what day? Weigh how much? Be a girl or boy?</p>
+        <p>See the guesses below!</p>
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
